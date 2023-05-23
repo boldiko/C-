b12f6999fe41d4daa55570fe683396f9a9d6f28d
unknown <boldinini38@mail.ru>
2023-05-23 15:26:01 +0300

    first commit

---
+++ HT_1_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+ if (a % 2 == 0)
+ {
+    Console.WriteLine("Да");
+    Console.Read();
+ }
+ else 
+ {
+    Console.WriteLine("Нет");
+    Console.Read();
+ }
+  
---
+++ HT_4_3/Program.cs
@@ -0,0 +1,7 @@
+﻿int[] randomArray = new int[8];
+
+for (int i = 0; i < 8; i++)
+{
+randomArray[i] = new Random().Next(1,99);
+Console.Write(randomArray[i] + " ");
+}
